e2e60bb9e952c981f5083b5b8b2148642f6221aa
Kevin Fung <kevinfung87@gmail.com>
2017-07-08 15:26:26 +0200

    first setup

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require ('express'),
+    morgan = require ('morgan'),
+    hbs = require ('hbs');
+
+var app = express();
+
+app.use(express.static('public'));
+app.use(morgan('dev'));
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+
+
+
+
+app.listen(3000, () => {
+  console.log('Web server started on port 3000');
+});
